b7420c400512455c24cb399b0a295034dd398e20
Thais Lara <thaiscostalara@gmail.com>
2026-04-17 04:05:13 -0300

    refactor: redefine campos obrigatórios para criação de novos clientes

--- app/Http/Requests/StoreClientRequest.php
+++ app/Http/Requests/StoreClientRequest.php
@@ -15,7 +15,7 @@ class StoreClientRequest extends FormRequest
     protected function prepareForValidation()
     {
         $enviados = array_keys($this->all());
-        $permitidos = ['nome', 'documento', 'documento_tipo'];
+        $permitidos = ['nome', 'documento', 'documento_tipo', 'email', 'data_nascimento', 'telefone', 'endereco'];
         $intrusos = array_diff($enviados, $permitidos);
 
         if (! empty($intrusos)) {
@@ -30,9 +30,32 @@ class StoreClientRequest extends FormRequest
     {
         return [
             'nome' => ['required', 'string', 'max:255'],
-            // unique comentado para testar sem banco:
-            'documento' => ['required', 'string', 'max:14' /* , 'unique:clients' */],
+            'email' => 'required|email|unique:clientes,email',
+            'documento' => ['required', 'string', 'max:14', 'unique:clientes,documento'],
             'documento_tipo' => ['required', 'in:CPF,CNPJ'],
+            'data_nascimento' => ['required', 'date_format:d/m/Y'],
+            'telefone' => ['nullable', 'string'],
+            'endereco' => ['nullable', 'string'],
+        ];
+    }
+
+    // Padronização de mensagens
+    public function messages(): array
+    {
+        return [
+            'nome.required' => 'O nome do cliente é obrigatório para o cadastro.',
+
+            'email.required' => 'Precisamos de um e-mail para enviar as atualizações da OS.',
+            'email.email' => 'O formato do e-mail informado não é válido.',
+            'email.unique' => 'Este e-mail já pertence a outro cliente em nosso sistema.',
+
+            'documento.required' => 'O número do documento (CPF ou CNPJ) é indispensável.',
+            'documento.unique' => 'Já existe um cliente cadastrado com este CPF/CNPJ.',
+            'documento.max' => 'O documento não pode ter mais que 14 dígitos.',
+
+            'documento_tipo.in' => 'Por favor, selecione apenas entre as opções: CPF ou CNPJ.',
+
+            'data_nascimento.date' => 'A data de nascimento deve estar em um formato de data válido.',
         ];
     }
 }
